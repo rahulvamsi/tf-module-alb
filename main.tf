27818894a41c0a70eabfa89934fc9d7639d9b583
Raghu K <raghuk.vit@gmail.com>
2022-09-17 08:24:11 +0530

    Update terraform-modules/tf-module-alb/

---
+++ main.tf
@@ -0,0 +1,58 @@
+resource "aws_lb" "public" {
+  name               = "roboshop-${var.env}-public"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.public.id]
+  subnets            = var.public_subnets
+
+  tags = {
+    Environment = "roboshop-${var.env}-public"
+  }
+}
+
+resource "aws_security_group" "public" {
+  name        = "${var.env}-public-alb"
+  description = "${var.env}-public-alb"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.env}-public-alb"
+  }
+}
+
+resource "aws_lb" "private" {
+  name               = "roboshop-${var.env}-private"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.private.id]
+  subnets            = [var.vpc_cidr_block]
+
+  tags = {
+    Environment = "roboshop-${var.env}-private"
+  }
+}
+
+resource "aws_security_group" "private" {
+  name        = "${var.env}-private-alb"
+  description = "${var.env}-private-alb"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+  tags = {
+    Name = "${var.env}-private-alb"
+  }
+}
+
---
+++ vars.tf
@@ -0,0 +1,6 @@
+variable "env" {}
+variable "vpc_id" {}
+variable "vpc_cidr_block" {}
+variable "public_subnets" {}
+variable "app_subnets" {}
+
