80ac6a3d3f61b6ea541ca543a933f6774b7bf716
Raghu K <raghuk.vit@gmail.com>
2022-09-21 06:24:16 +0530

    Update terraform-modules/tf-module-alb/

--- main.tf
+++ main.tf
@@ -35,14 +35,18 @@ resource "aws_lb_listener" "public-http" {
   port              = "80"
   protocol          = "HTTP"
 
+  //  default_action {
+  //    type = "redirect"
+  //
+  //    redirect {
+  //      port        = "443"
+  //      protocol    = "HTTPS"
+  //      status_code = "HTTP_301"
+  //    }
+  //  }
   default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.public.arn
   }
 }
 
