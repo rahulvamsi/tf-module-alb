2f52f6b4faa411de501a9f59afeb1710dba9a47f
Raghu K <raghuk.vit@gmail.com>
2022-09-21 07:48:02 +0530

    Update terraform-modules/tf-module-alb/

--- main.tf
+++ main.tf
@@ -65,6 +65,12 @@ resource "aws_security_group" "public" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "${var.env}-public-alb"
   }
