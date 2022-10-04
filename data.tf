bee29de47c9b48f379f3c56aea67a813eb3c7bae
rahulvamsi <p.rahulvamsi@gmail.com>
2022-10-04 19:54:50 +0530

    updated

--- data.tf
+++ data.tf
@@ -1,10 +1,10 @@
 data "aws_route53_zone" "public" {
-  name         = "devopsb66.online"
+  name         = "rahulsdevops.online"
   private_zone = false
 }
 
 data "aws_acm_certificate" "public" {
-  domain   = "*.devopsb66.online"
+  domain   = "*.rahulsdevops.online"
   statuses = ["ISSUED"]
 }
 
